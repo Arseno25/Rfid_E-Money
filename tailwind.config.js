ffef56da1a9c159ae2c034392db49e289a87e0d0
Arseno25 <utomnestagumara@gmail.com>
2023-12-31 18:36:44 +0700

    frontend modif

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,5 +6,8 @@ export default {
         './app/Filament/**/*.php',
         './resources/views/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
     ],
 }

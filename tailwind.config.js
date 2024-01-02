c895458101cf75c1770aff494d12c0fe786e2040
Arseno25 <utomnestagumara@gmail.com>
2024-01-02 11:29:44 +0700

    add error handler page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,8 @@ import preset from './vendor/filament/support/tailwind.config.preset'
 export default {
     presets: [preset],
     content: [
-        './app/Filament/**/*.php',
-        './resources/views/**/*.blade.php',
-        './vendor/filament/**/*.blade.php',
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+        "./app/Filament/**/*.php",
+        "./resources/views/**/*.blade.php",
+        "./vendor/filament/**/*.blade.php",
     ],
-}
+};

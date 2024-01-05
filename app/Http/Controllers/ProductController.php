2973259ce737f14c145037f6533fe0001597e0ab
Arseno25 <utomnestagumara@gmail.com>
2024-01-05 21:56:22 +0700

    add search

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,8 +10,18 @@ class ProductController extends Controller
 {
     public function index()
     {
-
         $product = Product::where('stock', '>', 0)->where('is_enabled', 1)->paginate(10);
         return view('products', ['product' => $product]);
+    }
+
+    public function search(Request $request)
+    {
+        $search = $request->input('query');
+
+        $product = Product::where('name', 'like', "%" . $search . "%")
+            ->where('stock', '>', 0)
+            ->where('is_enabled', 1)
+            ->paginate(10);
+        return view('products', ['product' => $product, 'search' => $search]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [\App\Http\Controllers\ProductController::class, 'index'])->name('home');
+Route::get('/search', [\App\Http\Controllers\ProductController::class, 'search'])->name('search');
